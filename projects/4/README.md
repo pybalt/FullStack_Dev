6778716809a29f72dcbb446b42f5686a6d7275c7
Leonel Baltazar Bravo <leonelbbravo@gmail.com>
2022-05-30 16:57:04 -0300

    Curso Buenas Practicas JS + Proyecto manipulacion JSON

---
+++ projects/4/README.md
@@ -0,0 +1,3 @@
+En este proyecto simplemente voy a manipular datos con un archivo JSON a traves de JS.
+Necesito ver correlatividades entre materias, y hacerlo a mano me llevaría mucho tiempo, de manera que
+lo haremos automatizado.
---
+++ projects/4/app.js
@@ -0,0 +1,11 @@
+/* In this project we will calculate the correlatives of the subjects of a student.
+    * The correlatives are the grades of the subjects that are related to the subject of the student.
+    * In order to course a subject, you need to approve the correlatives that are related to the subject.
+    * If you have approved all the correlatives, you can course the subject.
+    * We are going to read a JSON file (materias.json) that contains the subjects and the correlatives. 
+    * We will use the JSON.parse() method to convert the JSON string into a JavaScript object.
+    * We will use the forEach() method to iterate through the object.
+    * We will use the prompt() method to ask the user to enter the name of the subject.
+    * We will use the if() statement to check if the subject is in the object.
+    * If the subject is in the object, we will use the forEach() method to iterate through the correlatives.
+    * We are going to create a new array that contains the correlatives of a subject. */
