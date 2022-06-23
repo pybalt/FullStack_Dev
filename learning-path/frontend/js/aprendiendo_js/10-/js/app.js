bd82aad5e5a0c36e733baf720a66df59cd25580d
Leonel Baltazar Bravo <leonelbbravo@gmail.com>
2022-06-22 22:33:06 -0300

    ordenar arreglos

---
+++ learning-path/frontend/js/aprendiendo_js/10-/js/app.js
@@ -0,0 +1,9 @@
+/* Este codigo se trata de ordenar arreglos.
+* sort() => Ordena alfabeticamente
+* reverse() => Ordena el array de atras hacia adelante
+*/
+var aArregloAOrdenar = ["A", "C", "B", "Z"]
+console.log(aArregloAOrdenar.reverse())
+// [Z, B, C, A]
+console.log(aArregloAOrdenar.sort())
+// [A, B, C, Z]
