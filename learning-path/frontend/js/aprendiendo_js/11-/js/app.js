4ca10a096fd4e667eb2eb42bbe9a0bab0e31ff58
Leonel Baltazar Bravo <leonelbbravo@gmail.com>
2022-06-22 22:34:19 -0300

    crear variables a partir d desestructurar arreglos

---
+++ learning-path/frontend/js/aprendiendo_js/11-/js/app.js
@@ -0,0 +1,9 @@
+/* Este codigo se trata de desestructurar arreglos.
+* Desectructurar un arreglo es crear variables a partir de un arreglo.
+* 
+*/
+var aArregloEjemplo = Array.of(0, 1, 2)
+
+var [numero1, numero2, numero3] = aArregloEjemplo
+
+console.log(numero1, numero2, numero3)
