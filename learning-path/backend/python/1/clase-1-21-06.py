e511ba36f97d535788ae02fa189786dd40ce96ed
Leonel Baltazar Bravo <leonelbbravo@gmail.com>
2022-06-23 17:53:51 -0300

    color sight

--- learning-path/backend/python/1/clase-1-21-06.py
+++ learning-path/backend/python/1/clase-1-21-06.py
@@ -1,10 +1,10 @@
 ##El contenido de este archivo trata de la clase nro.1 del modulo back-end del curso FULLSTACK ITBA.
-## En esta primer clase trabajamos creando un virtual environment. 
+#* En esta primer clase trabajamos creando un virtual environment. 
 # Para esto, debemos situarnos en la ruta correspondiente
 # y lanzar el comando para crear la carpeta de virtualenvironment. 
 # py -m venv [ruta\foldername]
 # foldername es el nombre del virtualenviroment, idealmente venv. 
-# Entonces el codigo seria py -m venv [ruta\venv]
-# Luego activamos el venv. Con el siguiente codigo => [ruta/venv/Scripts/activate]
+#? Entonces el codigo seria py -m venv [ruta\venv]
+#? Luego activamos el venv. Con el siguiente codigo => [ruta/venv/Scripts/activate]
 # Debe de aparecer (env) y luego la ruta. Asi => (env)[ruta/venv]
-# Para desactivar el venv. Utilizamos el comando => deactivate.
+#? Para desactivar el venv. Utilizamos el comando => deactivate.
