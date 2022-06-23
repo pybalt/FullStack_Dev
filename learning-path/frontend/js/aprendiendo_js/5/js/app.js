394db7868fc1af0f3a239a42ad5867cc2c2643e2
Leonel Baltazar Bravo <leonelbbravo@gmail.com>
2022-06-22 22:22:51 -0300

    Arreglos multidimensionales

--- learning-path/frontend/js/aprendiendo_js/5/js/app.js
+++ learning-path/frontend/js/aprendiendo_js/5/js/app.js
@@ -1,1 +1,18 @@
-// 
+/* Este codigo trata sobre arreglos multidimensionales.
+* Un arreglo multidimensional no es mas que un arreglo que contiene tantos
+* arreglos como dimensiones.
+* Por ejemplo => array2Dimensiones = [ Dimension1[], Dimension2[] ] 
+*/
+var aPrimeraDimension = [0, 1, 2, 3]
+var aSegundaDimension = [0, 1, 2, 3]
+
+var aArregloDosDimensiones = [aPrimeraDimension, aSegundaDimension]
+/* Para acceder al contenido de un arreglo multimensional, 
+* debemos indicar el index de la dimension
+* y luego debemos indicar el index del elemento
+* dentro de la dimension seleccionada
+*/
+console.log(aArregloDosDimensiones)
+console.log(aArregloDosDimensiones[0][1])
+console.log(aArregloDosDimensiones[0][3])
+console.log(aArregloDosDimensiones[1][3])
