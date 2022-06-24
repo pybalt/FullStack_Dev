9535b73ffb6b0bb4d38fe2820d69f47f8df823b4
Leonel Baltazar Bravo <leonelbbravo@gmail.com>
2022-06-24 20:26:34 -0300

    Ejercicio 3 sprint4 semana1

--- learning-path/backend/python/3/ejercicios-sprint4-semana1.py
+++ learning-path/backend/python/3/ejercicios-sprint4-semana1.py
@@ -17,7 +17,21 @@ def function():
     else:
         print("Introduzca un numero entre 1 y 10000000")
         function()
-
+# *Ingresando un numero entero n por pantalla. 
+# *Para todos los numero enteros no negativos i < n, imprimir i2
+import math
+def functionA():
+    n = int(input("Ingrese un numero entero --> "))
+    if 1<=n<=20:
+        numeros = []
+        for n in range(n+1):
+            numeros.append(n)
+        else:
+            for n in numeros:
+                print(int(math.pow(n,2)))
+    else:
+        print("Debe de ingresar un numero entre 1 y 20")
+        functionA()
 
 if __name__ == "__main__":
-    function()
+    functionA()
