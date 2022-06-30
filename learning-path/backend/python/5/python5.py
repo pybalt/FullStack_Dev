b59d02d4d1e3ccadd6fadeb5743db1f3fc1642cf
Leonel Baltazar Bravo <leonelbbravo@gmail.com>
2022-06-30 20:11:06 -0300

    Ejercicio terminado. Agenda CSV

--- learning-path/backend/python/5/python5.py
+++ learning-path/backend/python/5/python5.py
@@ -8,11 +8,21 @@
 # --> nombre, apellido, telefono, tipo (laboral, personal)
 import csv
 
+runtime = True
+bienvenida = """Bienvenido a la agenda. 
+    Ingrese las opciones para navegar a traves de los menus: 
+    1.Mostrar todos los telefonos.
+    2.Buscar un telefono por nombre.
+    3.Agendar nuevo telefono.
+    4.Volver al menu inicial.
+    5.Salir de la agenda."""
 agendatitulos = ['Nombre', 'Apellido', 'Numero', 'Tipo']
 agenda = []
 
+
 def buscarCoincidencia(nombre, apellido):
     coincidencia=[]
+    numero_encontrado = False
     for i in range(len(agenda)):
         if nombre == agenda[i][0] and apellido == agenda[i][1]:
             coincidencia.append(True)
@@ -20,43 +30,23 @@ def buscarCoincidencia(nombre, apellido):
             break
         else:
             coincidencia.append(False)
+            numero_encontrado=False
     return coincidencia, numero_encontrado
 
 def formatear(var_a_formatear):
     var_a_formatear= var_a_formatear.title().replace(" ","")
     return var_a_formatear
 
-def menu0():
-    print("""Bienvenido a la agenda. 
-    Ingrese las opciones para navegar a traves de los menus: 
-    1.Mostrar todos los telefonos.
-    2.Buscar un telefono por nombre.
-    3.Agendar nuevo telefono.
-    4.Volver al menu inicial.
-    5.Salir de la agenda.""")
-    numero = input('Seleccione el menu. --> ')
-    numero = int(numero)
-    if numero == 1:
-        menu1()
-    elif numero == 2:
-        menu2()
-    elif numero == 3:
-        menu3()
-    elif numero == 4:
-        menu0()
-    elif numero == 5:
-        return agenda
-    else:
-        print("Has ingresado una opcion erronea, regresando al menu principal...")
-        menu0()
-
-
 def menu1():
     print("""Mostrando todos los telefonos...""")
-    for index in range(len(agenda)):
-        print(f"""--><-- Contacto {index+1} --><--""")
-        for i in range(len(agendatitulos)):
-            print(f"""{agendatitulos[i]}: {agenda[index][i]}""")
+    if len(agenda)==0:
+        print("Agenda vacia")
+    else:
+        for index in range(len(agenda)):
+            print(f"""--><-- Contacto {index+1} --><--""")
+            for i in range(len(agendatitulos)):
+                print(f"""{agendatitulos[i]}: {agenda[index][i]}""")
+    return agenda
 
 
 def menu2():
@@ -67,19 +57,22 @@ def menu2():
     if any(coincidencia): 
         print(f"Contacto encontrado. Su numero es {numero_encontrado}")
     else:
-        agregarcontacto=input("Contacto no encontrado. Desea agregarlo? S/N")
-        if agregarcontacto=='S' or 's':
-            menu3()
+        agregarcontacto=input("Contacto no encontrado. Desea agregarlo? S/N --> ")
+    return agregarcontacto
 
 def menu3():
-    print("Agendar nuevo telefono.")
-    nombre = formatear(str(input("Ingrese el nombre --> ")))
-    apellido = formatear(str(input("Ingrese el apellido --> ")))
-    numero = ''.join(filter(str.isnumeric, input("Ingrese el numero --> ").replace(" ",""))) #Este codigo evita introducir espacios y letras en el string numero.
-    numero = int(numero) # Este metodo convierte el string en numero int.
-    tipo = formatear(str(input("Bajo que etiqueta desea guardar este contacto? Ej: Trabajo/Personal --> ")))
-    array = [nombre, apellido, numero, tipo]
-    agenda.append(array)
+    seguir = 's'
+    while seguir == 's' or 'S':
+        print("Agendar nuevo telefono.")
+        nombre = formatear(str(input("Ingrese el nombre --> ")))
+        apellido = formatear(str(input("Ingrese el apellido --> ")))
+        numero = ''.join(filter(str.isnumeric, input("Ingrese el numero --> ").replace(" ",""))) #Este codigo evita introducir espacios y letras en el string numero.
+        numero = int(numero) # Este metodo convierte el string en numero int.
+        tipo = formatear(str(input("Bajo que etiqueta desea guardar este contacto? Ej: Trabajo/Personal --> ")))
+        array = [nombre, apellido, numero, tipo]
+        agenda.append(array)
+        seguir= input("Desea seguir agregando nuevos contactos? S/N")
+    return agenda
 
 if __name__ == '__main__':
 
@@ -88,11 +81,28 @@ if __name__ == '__main__':
         agendareader = csv.reader(csvfile, delimiter=';')
         for row in agendareader:
             agenda.append(row)
-    menu0()
+    
+    while runtime: 
+        print(bienvenida)
+        numero = ''.join(filter(str.isnumeric, input("Ingrese el numero --> ").replace(" ",""))) #Este codigo evita introducir espacios y letras en el string numero.
+        if numero ==1:
+            menu1()
+            continue
+        elif numero == 2:
+            agregarcontacto = menu2()
+            if agregarcontacto == 's' or agregarcontacto == 'S':
+                menu3()
+            continue
+        elif numero == 3:
+            menu3()
+            continue
+        elif numero == 5:
+            runtime = False
+        else:
+            continue
 
-    with open(path, 'w') as csvfile:
+    with open(path, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=';')
         for row in agenda:
             writer.writerow(row)
         csvfile.close()
-    #! Ver. Este fragmento de codigo dispara errores.
